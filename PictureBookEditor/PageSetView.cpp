a53d00d1b0b256dc01c556f93c6a17cfa443a15d
mita <cpjvmm@outlook.jp>
2020-11-09 10:40:19 +0900

    add: PageSetView PageView prototype

--- PictureBookEditor/PageSetView.cpp
+++ PictureBookEditor/PageSetView.cpp
@@ -4,6 +4,8 @@ constexpr auto LEFT_X = 40;
 constexpr auto TOP_Y = 150;
 constexpr auto BACK_WID = 2500;
 constexpr auto BACK_HIGH = 1700;
+constexpr auto RIGHT_X = LEFT_X + BACK_WID;
+constexpr auto BOTTOM_Y = TOP_Y + BACK_HIGH;
 
 constexpr auto B_POS_X = 600;
 constexpr auto B_WID = 200;
@@ -13,22 +15,35 @@ constexpr auto B_POS_Y = 1950;
 constexpr auto F_SIZE = 50;
 constexpr auto B_OFFSET = 40;
 
+constexpr auto EXPANSION = 1.5;
+
 void PageSetView::pollEvent()
 {
 	back_rect.draw(Palette::White).drawFrame(4, 4, Palette::Lightsalmon);
 	pollButtonEvent();
+	pollPageEvent();
 }
 
 void PageSetView::init()
 {
 	back_rect = Rect(LEFT_X, TOP_Y, BACK_WID, BACK_HIGH);
 	initButton();
+	initPageView();
 }
 
 void PageSetView::initButton()
 {
 	prev_btn = std::make_shared<MyButton>(MyButton(B_POS_X, B_POS_Y, B_WID, B_HIGH, String(U"ëOï≈"), F_SIZE));
 	next_btn = std::make_shared<MyButton>(MyButton(B_POS_X + B_GAP, B_POS_Y, B_WID, B_HIGH, String(U"éüçÄ"), F_SIZE));
+}
+
+void PageSetView::initPageView()
+{
+	max_x = Window::ClientWidth();
+	max_y = Window::ClientHeight();
+	absolute_pos = Vec2(LEFT_X, TOP_Y);
+	place = Vec2(0, 0);
+	font = Font(30);
 }
 
 void PageSetView::pollButtonEvent()
@@ -42,6 +57,116 @@ void PageSetView::pollButtonEvent()
 	{
 		Print << U"next page";
 		next_flag = true;
+	}
+}
+
+void PageSetView::pollPageEvent()
+{	
+	pollZoomEvent();
+	pollChangePlaceEvent();
+	pollChangeAbsPosEvent();
+	pollMoveRectEvent();
+	rectf.draw(isPressed ? Palette::Red : Palette::Gray);
+	font(place).draw(absolute_pos + (place + Vec2(0, -60)) * expansion, Palette::Orange);
+	Print << U"abs: {}, place: {}, max: ({}, {})"_fmt(absolute_pos, place, max_x, max_y);
+}
+
+void PageSetView::pollChangeAbsPosEvent()
+{
+	if (MouseR.pressed())
+	{
+		absolute_pos += Cursor::Delta();
+	}
+	if (absolute_pos.x < -max_x)
+	{
+		absolute_pos.x = -max_x;
+	}
+	else if (absolute_pos.x > max_x)
+	{
+		absolute_pos.x = max_x;
+	}
+	if (absolute_pos.y < -max_y)
+	{
+		absolute_pos.y = -max_y;
+	}
+	else if (absolute_pos.y > max_y)
+	{
+		absolute_pos.y = max_y;
+	}
+}
+
+void PageSetView::pollChangePlaceEvent()
+{
+	if (rectf.leftClicked())
+	{
+		isPressed = true;
+	}
+	if (MouseL.up())
+	{
+		isPressed = false;
+	}
+	if (isPressed)
+	{
+		place += Cursor::Delta() / expansion;
+	}
+
+	if (place.x < 0)
+	{
+		place.x = 0;
+	}
+	else if (place.x > max_x)
+	{
+		place.x = max_x;
+	}
+	if (place.y < 0)
+	{
+		place.y = 0;
+	}
+	else if (place.y > max_y)
+	{
+		place.y = max_y;
+	}
+}
+
+void PageSetView::pollZoomEvent()
+{
+	wheel = Mouse::Wheel();
+	if (wheel == 1)
+	{
+		if (expansion >= 0.25)
+		{
+			expansion = expansion / 1.5; 
+			absolute_pos = Window::ClientCenter();
+		}
+		else
+		{
+			expansion = 0.25;
+		}
+	}
+	else if (wheel == -1)
+	{
+		if (expansion <= 4)
+		{
+			expansion = expansion * EXPANSION;
+			absolute_pos = Window::ClientCenter();
+		}
+		else
+		{
+			expansion = 4;
+		}
+	}
+}
+
+void PageSetView::pollMoveRectEvent()
+{
+	cur_pos = absolute_pos + place * expansion;
+	if (cur_pos.x < LEFT_X || cur_pos.x > RIGHT_X || cur_pos.y < TOP_Y || cur_pos.y > BOTTOM_Y)
+	{
+		rectf = RectF(Arg::center(-1, -1), 0, 0);
+	}
+	else
+	{
+		rectf = RectF(Arg::center(cur_pos), 500 * expansion, 500 * expansion);
 	}
 }
 
--- PictureBookEditor/PageSetView.h
+++ PictureBookEditor/PageSetView.h
@@ -6,12 +6,25 @@
 class PageSetView : public ViewComponent
 {
 private:
+    Rect back_rect;
+
     std::shared_ptr<MyButton> prev_btn;
     std::shared_ptr<MyButton> next_btn;
     bool prev_flag = false;
     bool next_flag = false;
 
-    Rect back_rect;
+    Vec2 absolute_pos;
+    Vec2 place;
+    Vec2 cur_pos;
+    Point delta;
+    RectF rectf;
+    Font font;
+    bool isPressed = false;
+    double expansion = 1.0;
+    int wheel = 0;
+
+    int max_x = 0;
+    int max_y = 0;
 public:
     PageSetView(std::shared_ptr<Controller> ctr) : ViewComponent(ctr)
     {
@@ -23,6 +36,12 @@ public:
 private:
     void init();
     void initButton();
+    void initPageView();
     void pollButtonEvent();
+    void pollPageEvent();
+    void pollChangeAbsPosEvent();
+    void pollChangePlaceEvent();
+    void pollZoomEvent();
+    void pollMoveRectEvent();
 };
 
